7112a2becd8bf0a55b15409891b009b44c663f02
Rafael Freitas <rafaelevoinfo@gmail.com>
2026-06-30 20:56:28 -0300

    Adicionando suporte a upload de arquivos para o Cloudinary

--- Src/Application/Controllers/UploadController.cs
+++ Src/Application/Controllers/UploadController.cs
@@ -8,11 +8,13 @@ namespace ProximoTurnoApi.Application.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 [Authorize(Roles = Roles.Admin)]
-public class UploadController(ILogger<UploadController> logger, CloudinaryService _cloudinaryService) : ControllerBasico(logger) {
+public class UploadController(ILogger<UploadController> logger, CloudinaryService _cloudinaryService, IWebHostEnvironment _env) : ControllerBasico(logger) {
     
     [HttpGet("signature")]
-    public IActionResult GetSignature() {
-        var signature = _cloudinaryService.GetSignature("proximoturno/jogos");
+    public IActionResult GetSignature([FromQuery] string? tipo = null) {
+        var baseFolder = _env.IsDevelopment() ? "proximoturno/jogos_debug" : "proximoturno/jogos";
+        var folder = tipo == "manuais" ? $"{baseFolder}/manuais" : baseFolder;
+        var signature = _cloudinaryService.GetSignature(folder);
         return Ok(signature);
     }
 }
